550f98730c248ad9a166dc4ad730899c84379e54
4HuyNam12 <91261960+4HuyNam12@users.noreply.github.com>
2021-11-23 15:46:36 +0700

    clean code

--- Lesson_13/Information/src/Information.java
+++ Lesson_13/Information/src/Information.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class Information {
     Scanner sc = new Scanner(System.in);
+    int yearOfBirth;
 
     public String inputFullName() {
-        String fullName = sc.nextLine();
-        return fullName;
+        return sc.nextLine();
     }
 
     public int inputYearOfBirth() {
-        int yearOfBirth = 0;
         boolean ischeck = false;
         while (!ischeck) {
             try {
@@ -20,8 +19,7 @@ public class Information {
             } catch (MyException exceptions) {
                 System.out.println(exceptions.getMessage());
             } catch (NumberFormatException exceptions) {
-                System.out.println("Năm sinh phải là một số nguyên");
-                System.out.println("Mời bạn nhập lại năm sinh:");
+                System.out.println("Năm sinh phải là một số nguyên\nNhập lại năm sinh:");
             }
 //        }
         }
@@ -29,20 +27,19 @@ public class Information {
     }
 
     public String inputAddress() {
-        String address = sc.nextLine();
-        return address;
+        return sc.nextLine();
     }
 
     public void display(String fullName, int yearOfBirth, String address) {
         System.out.println("Họ và Tên: " + fullName + " - " + "Năm sinh: " + yearOfBirth + " - " + "Địa chỉ: " + address + ".");
     }
 
-    public void calculateAge(int yearOfBirth) {
+    public void calculateAge() {
         int age = 2021 - yearOfBirth;
         if (age >= 18) {
-            System.out.println("Bạn đã: " + age + "tuổi , Mời bạn đi bầu cử");
+            System.out.println("Bạn đã " + age + " tuổi . Mời bạn đi bầu cử.");
         } else {
-            System.out.println("Bạn chưa đủ 18 tuổi , Bạn có thể đi bầu cử sau khi đủ 18 tuổi");
+            System.out.println("Bạn chưa đủ 18 tuổi . Bạn có thể đi bầu cử sau khi đủ 18 tuổi.");
         }
     }
 }
--- Lesson_13/Information/src/Main.java
+++ Lesson_13/Information/src/Main.java
@@ -1,14 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        Information infor = new Information();
+        Information info = new Information();
         System.out.println("Nhập vào họ và tên: ");
-        String fullName = infor.inputFullName();
+        String fullName = info.inputFullName();
         System.out.println("Nhập vào năm sinh: ");
-        int yearOfBirth = infor.inputYearOfBirth();
+        int yearOfBirth = info.inputYearOfBirth();
         System.out.println("Nhập vào địa chỉ: ");
-        String address = infor.inputAddress();
-        infor.display(fullName, yearOfBirth, address);
-        infor.calculateAge(yearOfBirth);
+        String address = info.inputAddress();
+        info.display(fullName, yearOfBirth, address);
+        info.calculateAge();
     }
 }
